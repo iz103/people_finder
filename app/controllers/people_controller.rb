b34b72e168c5f3311af61aee6c249767a789c527
Iz El-Bahnasy <iz@deliverablesschedule.com>
2011-08-02 09:20:27 +1000

    tests

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -6,40 +6,4 @@ class PeopleController  < ActionController::Base
     @people = @search.all
   end
 
-  # def show
-  #   @property = Property.find(params[:id])
-  # end
-  # 
-  # def new
-  #   @property = Property.new
-  # end
-  # 
-  # def edit
-  #   @property = Property.find(params[:id])
-  # end
-  # 
-  # def create
-  #   @property = Property.new(params[:property])
-  #   if @property.save
-  #     redirect_to(properties_path, :notice => 'Property was successfully created.')
-  #   else
-  #     render :action => "new"
-  #   end
-  # end
-  # 
-  # def update
-  #   @property = Property.find(params[:id])
-  #   if @property.update_attributes(params[:property])
-  #      redirect_to(@property, :notice => 'Property was successfully updated.')
-  #    else
-  #      render :action => "edit"
-  #    end
-  # end
-  # 
-  # def destory
-  #   @property = Property.find(params[:id])
-  #   @property.destroy
-  #   redirect_to properties_path
-  # end
-
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Person do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @person_1 = Factory(:person, :email => "ck@hotmail.com")
+    @person_2 = Factory(:person, :email => "k@gmail.com")
+  end
+  it "searches for people with gmail accounts" do
+    results = Person.search(:email_contains => "gmail").all
+    results.should include(@person_1)
+  end
+
 end
